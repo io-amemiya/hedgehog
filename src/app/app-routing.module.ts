6c178e2dab2fde3e5a67e2c5ac97f52ad8d1632a
io-amemiya <a_amemiya@system-io.co.jp>
2019-02-11 23:17:10 +0900

    レイアウト修正

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,29 @@
 // Module
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 // Component
 import { MainMenuComponent } from '../app/pages/main-menu/main-menu.component';
+import { WorkingTimeMenuComponent } from '../app/pages/app-recog/working-time-menu/working-time-menu.component';
+import { WorkingTimeComponent } from '../app/pages/app-recog/working-time/working-time.component';
 
 const routes: Routes = [
   // メインページ
   { path: '', redirectTo: 'main-menu', pathMatch: 'full' },
-  { path: '**', redirectTo: 'main-menu', pathMatch: 'full' },
-  // サブページ
-  { path: 'main-menu', component: MainMenuComponent }
+  // メインメニュー
+  { path: 'main-menu', component: MainMenuComponent },
+  // 申請・承認メニュー
+  { path: 'working-time-menu', component: WorkingTimeMenuComponent },
+  { path: 'working-time', component: WorkingTimeComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
-  exports: [RouterModule]
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes, { useHash: true })
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
